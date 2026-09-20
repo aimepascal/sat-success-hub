9e7f547fe9def9f7ad21844c78ed334e00ab89af
Aime Pascal KOMEZUSENGE <komezusengeaimepascal@gmail.com>
2026-09-20 22:07:38 +0200

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { SiteHeader } from "@/components/SiteHeader";
 import { SiteFooter } from "@/components/SiteFooter";
+import { AITutorChat } from "@/components/AITutorChat";
 import { supabase } from "@/integrations/supabase/client";
 import { registerAppServiceWorker } from "@/lib/pwa";
 
@@ -114,6 +115,7 @@ function RootComponent() {
         </main>
         <SiteFooter />
       </div>
+      <AITutorChat />
       <Toaster position="top-center" richColors />
     </QueryClientProvider>
   );
